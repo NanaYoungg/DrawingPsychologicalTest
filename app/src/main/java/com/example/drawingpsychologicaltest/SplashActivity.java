ddf40c342d59322d0941f1b3c29082834282b34f
ny0414 <dudtnsl1004@naver.com>
2020-12-16 02:08:22 +0900

    add splash

--- app/src/main/java/com/example/drawingpsychologicaltest/SplashActivity.java
+++ app/src/main/java/com/example/drawingpsychologicaltest/SplashActivity.java
@@ -1,4 +1,82 @@
 package com.example.drawingpsychologicaltest;
 
-public class SplashActivity {
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler mHadler = new Handler();
+    private View mDecorView;
+    private int	mUiOption;
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //상단 상태바 없애기 onCreate아래 바로 작성
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        //하단 소프트키 없애기
+        mDecorView = getWindow().getDecorView();
+        mUiOption = getWindow().getDecorView().getSystemUiVisibility();
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH )
+            mUiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
+            mUiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
+            mUiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        mDecorView.setSystemUiVisibility( mUiOption );
+
+        //스플래쉬 핸들러
+        mHadler.postDelayed(new SplashHandler(), 4000);
+
+        final LottieAnimationView mANIMATION_VIEW = (LottieAnimationView) findViewById(R.id.splash_lottie);
+        mANIMATION_VIEW.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+
+    }
+
+    private class SplashHandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+
+    }
+    //스플래시 화면에서 넘어갈때 뒤로가기 막기
+    public void onBackPressed(){
+
+    }
 }
