8fee5686b4878cf77f4d654c78ab9ba791903dc9
ny0414 <dudtnsl1004@naver.com>
2020-12-16 02:57:30 +0900

    add scroll view

--- app/src/main/java/com/example/drawingpsychologicaltest/MainActivity.java
+++ app/src/main/java/com/example/drawingpsychologicaltest/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.drawingpsychologicaltest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private View mDecorView;
+    private int	mUiOption;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mDecorView = getWindow().getDecorView();
+        mUiOption = getWindow().getDecorView().getSystemUiVisibility();
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH )
+            mUiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
+            mUiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
+            mUiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        mDecorView.setSystemUiVisibility( mUiOption );
     }
 }
